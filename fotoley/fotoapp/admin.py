a016b79ab9a4e4699ca59214a0d8abaa578e3887
Aryan <agarwalaryan139@gmail.com>
2022-10-06 12:36:28 +0530

    user model + profile created

--- fotoley/fotoapp/admin.py
+++ fotoley/fotoapp/admin.py
@@ -1,3 +1,22 @@
+from email.headerregistry import Group
 from django.contrib import admin
 
+from django.contrib.auth.models import User, Group
 # Register your models here.
+from .models import Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'email', 'password', 'last_login', 'date_joined']
+    ordering = ('username',)
+    inlines = (ProfileInline,)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
