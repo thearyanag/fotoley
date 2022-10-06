5bdf0636d6f22c506a2ed7e06218ee34c0c03133
Aryan <agarwalaryan139@gmail.com>
2022-10-06 13:40:38 +0530

    added signals to automatially associate a profile

--- fotoley/fotoapp/models.py
+++ fotoley/fotoapp/models.py
@@ -11,3 +11,15 @@ class Profile(models.Model):
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False , blank=True)
     def __str__(self):
         return self.user.username
+
+# signal to create profile when user is created
+from django.db.models.signals import post_save
+def create_profile(sender, instance ,created ,**kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(user_profile)
+        user_profile.save()
+        
+post_save.connect(create_profile, sender=User)
+
